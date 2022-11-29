91cc949b44ae57f6e2e364bd6fc7ab05022bdd16
“cathy-h-04“ <cathy.sn.h@gmail.com>
2022-11-29 00:23:55 -0500

    testing SQL

--- celeb_testing.py
+++ celeb_testing.py
@@ -1,30 +1,62 @@
 # Credit for the below code: https://forum.freecodecamp.org/t/extracting-data-from-json/452527
 # Profiler guesses your age, gender and nationality
-import requests
+
 import json
+import os
+
+import requests
+
+
+#from flask import Flask, flash, redirect, render_template, request, session
+#from flask_session import Session
+#from tempfile import mkdtemp
+#app = Flask(__name__)
+
+import sqlite3
+from sqlite3 import Error
+
+def create_connection(path):
+    connection = None
+    try:
+        connection = sqlite3.connect("/Users/pzhang/Desktop/CS_Final_Project/CelebritiesAndPersonalities/celebs.db")
+        print("Connection to SQLite DB successful")
+    except Error as e:
+        print("The error occurred")
+
+    return connection
+
+connection = create_connection("E:\\celebs.db")
+
+db = connection.cursor()
+
+#db.execute("CREATE TABLE celebs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, MBTI TEXT NOT NULL, enne TEXT NOT NULL, points NUMERIC)")
+
+
+
 
 # User's input
 # user_name = input("Input your first name: ")
 
 # Nationality
-celebs = requests.get('https://api.personality-database.com/api/v1/profiles?offset=1&limit=100&pid=1&sort=top&property_id=1')
-# gender = requests.get('https://api.genderize.io/?name='+user_name)
-# age = requests.get('https://api.agify.io/?name='+user_name)
+
+celebs = requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
 
 celeb_data = (celebs.json()["profiles"])
-# age_data = gender.json()['gender']
-# gen_data = age.json()['age']
 
-celeb_name = celeb_data[0]["mbti_profile"]
 
-# print(new_data)
-# print('gender: ', age_data)
-# print('age: ', gen_data)
+print(len(celeb_data))
 
-for i in range(100):
-    celeb_name = celeb_data[i]["mbti_profile"]
-    celeb_personality = celeb_data[i]["personality_type"]
+#for i in range(100):
+for celeb in celeb_data:
+    celeb_name = celeb["mbti_profile"]
+    celeb_personality = celeb["personality_type"]
+    mbti = celeb_personality.split()[0]
+    enne = celeb_personality.split()[1]
+    print(celeb_name,mbti,enne)
+    db.execute("INSERT into celebs (name, MBTI, enne, points) VALUES (?, ?, ?, ?)", celeb_name, mbti, enne, '0')
+
+
     
-    print(celeb_name, celeb_personality)
+    #print(celeb_name, celeb_personality)
     
 
