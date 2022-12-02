b78324c9b9133a71e3b0b61550d8e1d1601da5a9
“cathy-h-04“ <cathy.sn.h@gmail.com>
2022-12-01 21:47:35 -0500

    SQL works.

--- celeb_testing.py
+++ celeb_testing.py
@@ -18,7 +18,8 @@ from sqlite3 import Error
 def create_connection(path):
     connection = None
     try:
-        connection = sqlite3.connect("/Users/pzhang/Desktop/CS_Final_Project/CelebritiesAndPersonalities/celebs.db")
+        connection = sqlite3.connect("os.path.basename(celebs.db)")
+        #connection = sqlite3.connect("/Users/pzhang/Desktop/CS_Final_Project/CelebritiesAndPersonalities/celebs.db")
         print("Connection to SQLite DB successful")
     except Error as e:
         print("The error occurred")
@@ -29,36 +30,57 @@ connection = create_connection("E:\\celebs.db")
 
 db = connection.cursor()
 
-
-#db.execute("CREATE TABLE celebs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, MBTI TEXT NOT NULL, enne TEXT NOT NULL, points NUMERIC)")
-
-# User's input
-# user_name = input("Input your first name: ")
-
-# Nationality
+db.execute("CREATE TABLE celebs (id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, name TEXT NOT NULL, MBTI TEXT NOT NULL, enne TEXT NOT NULL, points NUMERIC)")
 
 celebs = pip._vendor.requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
-
+characters = pip._vendor.requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
 
 celeb_data = (celebs.json()["profiles"])
+character_data = (characters.json()["profiles"])
+
+data = celeb_data + character_data
 
 
-print(len(celeb_data))
+#data = [{}]
+#data.append(celebs.json()["profiles"])
+#data.append(characters.json()["profiles"])
+#print(data)
 
-#for i in range(100):
-for celeb in celeb_data:
-    celeb_name = celeb["mbti_profile"]
-    celeb_personality = celeb["personality_type"]
-    mbti = celeb_personality.split()[0]
-    enne = celeb_personality.split()[1]
-    print(celeb_name,mbti,enne)
-    db.execute("INSERT INTO celebs (name, MBTI, enne, points) VALUES (?, ?, ?, ?)", (celeb_name, mbti, enne, '0'))
+
+
+#print(len(celeb_data))
+
+# for i in range(100):
+# for celeb in celeb_data:
+#     celeb_personality = celeb["personality_type"]
+#     mbti = celeb_personality.split()[0]
+#     enne = celeb_personality.split()[1]
+#     print(celeb_name,mbti,enne)
+#     db.execute("INSERT INTO celebs (name, MBTI, enne, points) VALUES (?, ?, ?, ?)", (celeb_name, mbti, enne, '0'))
+    
+#     connection.commit()
+
+
+    
+    #print(celeb_name, celeb_personality)
+    
+    
+for person in data:
+    person_name = person["mbti_profile"]
+    person_personality = person["personality_type"]
+    mbti = person_personality.split()[0]
+    enne = person_personality.split()[1]
+    print(person_name,mbti,enne)
+    db.execute("INSERT INTO celebs (name, MBTI, enne, points) VALUES (?, ?, ?, ?)", (person_name, mbti, enne, '0'))
     
     connection.commit()
 
 
     
     #print(celeb_name, celeb_personality)
+
+db.execute("DROP TABLE celebs")
+
 db.close()
     
 
