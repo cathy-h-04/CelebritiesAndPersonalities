d4688a2899c91820056db1121e40b3d6b6b2f30a
“cathy-h-04“ <cathy.sn.h@gmail.com>
2022-12-06 14:34:28 -0500

    finished setup

--- setup.py
+++ setup.py
@@ -1,133 +1,111 @@
 
-import os
+# #THIS IS HOW WE CREATED THE DATA
 
-import sqlite3
-from sqlite3 import Error
+# import os
 
-# from cs50 import SQL
-from flask import Flask, flash, redirect, render_template, request, session
+# import sqlite3
+# from sqlite3 import Error
 
-from flask_session import Session
+# # from cs50 import SQL
+# from flask import Flask, flash, redirect, render_template, request, session
+
 # from flask_session import Session
-from tempfile import mkdtemp
+# # from flask_session import Session
+# from tempfile import mkdtemp
 
-from werkzeug.security import check_password_hash, generate_password_hash
-from datetime import datetime
+# from werkzeug.security import check_password_hash, generate_password_hash
+# from datetime import datetime
 
-# importing helper functions
-from helpers import apology, login_required
+# # importing helper functions
+# from helpers import apology, login_required
 
-import json
+# import json
 
-from pip._vendor import requests
-#from tempfile import mkdtemp
-#app = Flask(__name__)
+# from pip._vendor import requests
+# #from tempfile import mkdtemp
+# #app = Flask(__name__)
 
-import sqlite3
-from sqlite3 import Error
-
-# FIRST CHECK:
-    
-#     TYPE: 
-#     sqlite3 database.db
-#     .schema
-    
-#     Should see:
+# import sqlite3
+# from sqlite3 import Error
 
 
-
-# CREATE TABLE users (
-#                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-#                 email TEXT NOT NULL, 
-#                 password TEXT NOT NULL, 
-#                 maiden TEXT NOT NULL, 
-#                 nickname TEXT NOT NULL
-#             );
+# # CREATE TABLE users (
+# #                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+# #                 email TEXT NOT NULL, 
+# #                 password TEXT NOT NULL, 
+# #                 maiden TEXT NOT NULL, 
+# #                 nickname TEXT NOT NULL
+# #             );
 
 
 
-# CREATE TABLE points (
-#                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-#                 celeb_id INTEGER NOT NULL,
-#                 user_id INTEGER NOT NULL,
-#                 points NUMERIC,
-#                 FOREIGN KEY(celeb_id) REFERENCES celebs(id),
-#                 FOREIGN KEY(user_id) REFERENCES users(id)
-#             );
+# # CREATE TABLE points (
+# #                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+# #                 celeb_id INTEGER NOT NULL,
+# #                 user_id INTEGER NOT NULL,
+# #                 points NUMERIC,
+# #                 FOREIGN KEY(celeb_id) REFERENCES celebs(id),
+# #                 FOREIGN KEY(user_id) REFERENCES users(id)
+# #             );
 
 
-# CREATE TABLE celebs (
-#                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
-#                 name TEXT NOT NULL, 
-#                 MBTI TEXT NOT NULL, 
-#                 enne TEXT NOT NULL,
-#                 nationality TEXT,
-#                 gender TEXT,
-#                 age NUMBER
-#             );
+# # CREATE TABLE celebs (
+# #                 id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, 
+# #                 name TEXT NOT NULL, 
+# #                 MBTI TEXT NOT NULL, 
+# #                 enne TEXT NOT NULL,
+# #                 nationality TEXT,
+# #                 gender TEXT,
+# #                 age NUMBER
+# #             );
 
 
-#EXECUTE THIS ONCE AT THE VERY BEGGINNING. 
-
-connection = sqlite3.connect("database.db", check_same_thread=False)
-
-db = connection.cursor()
-
-# db.execute("DELETE FROM points")
-# db.execute("DELETE FROM sqlite_sequence where name='points'")
-
-
-# db.execute("DELETE FROM users")
-# db.execute("DELETE FROM sqlite_sequence where name='users'")
-
-
-
-celebs = requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
-characters = requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
-
-celeb_data = (celebs.json()["profiles"])
-character_data = (characters.json()["profiles"])
-
-data = celeb_data + character_data
-
-#for person in data:
-
-for person in data:
-    
-    name_exists = True
-    
-    person_full_name = person["mbti_profile"]
-    person_name = person_full_name.split()[0]
-    person_personality = person["personality_type"]
-    mbti = person_personality.split()[0]
-    enne = person_personality.split()[1]
-    
-    person_nat = "N/A"
-    person_gen = "N/A"
-    person_age = "N/A"
-    
-    person_gender_search = requests.get('https://api.genderize.io/?name='+person_name)
-    person_age_search = requests.get('https://api.agify.io/?name='+person_name)
-    
-    if person_gender_search.json()['count'] == 0:
-        name_exists = False
-
-    if name_exists:
-        person_nationality_search = requests.get('https://api.nationalize.io/?name='+person_name)
-        person_nat = (person_nationality_search.json()['country'][0])['country_id']
-        person_gen = person_gender_search.json()['gender']
-        person_age = person_age_search.json()['age']
-
-    db.execute("INSERT INTO celebs (name, MBTI, enne, nationality, gender, age) VALUES (?, ?, ?, ?, ?, ?)", (person_full_name, mbti, enne, person_nat, person_gen, person_age))
-    connection.commit()
-
-    
-    
-# CHECK AND SEE IF THIS SUCESFULLY INPUTS INTO DATABASE! SHOULD HAVE 600 ROWS
-
-
-
-#MIGHT HAVE SOMEE ISSUES WITH THE THREAD. I BASICALLY COMMITED A NEW CONNECTION HERE BC ITS A DIFF FILE FROM APP.PY. IF THAT DOESNT WORK, TRY USING THE SAME CONNECTION BELLOW: 
 # connection = sqlite3.connect("database.db", check_same_thread=False)
+
 # db = connection.cursor()
+
+
+
+# # db.execute("DELETE FROM points")
+# # db.execute("DELETE FROM sqlite_sequence where name='points'")
+
+
+# # db.execute("DELETE FROM users")
+# # db.execute("DELETE FROM sqlite_sequence where name='users'")
+
+
+# #Accessed as much data as possible from the personality database
+
+# celebs = requests.get('https://api.personality-database.com/api/v1/profiles?offset=0&limit=100000&pid=1&sort=top&property_id=1')
+
+# data = (celebs.json()["profiles"])
+
+# for person in data:
+    
+#     name_exists = True
+    
+#     person_full_name = person["mbti_profile"]
+#     person_name = person_full_name.split()[0]
+#     person_personality = person["personality_type"]
+#     mbti = person_personality.split()[0]
+#     enne = person_personality.split()[1]
+    
+#     person_nat = "N/A"
+#     person_gen = "N/A"
+#     person_age = "N/A"
+    
+#     person_gender_search = requests.get('https://api.genderize.io/?name='+person_name)
+#     person_age_search = requests.get('https://api.agify.io/?name='+person_name)
+    
+#     if person_gender_search.json()['count'] == 0:
+#         name_exists = False
+
+#     if name_exists:
+#         person_nationality_search = requests.get('https://api.nationalize.io/?name='+person_name)
+#         person_nat = (person_nationality_search.json()['country'][0])['country_id']
+#         person_gen = person_gender_search.json()['gender']
+#         person_age = person_age_search.json()['age']
+
+#     db.execute("INSERT INTO celebs (name, MBTI, enne, nationality, gender, age) VALUES (?, ?, ?, ?, ?, ?)", (person_full_name, mbti, enne, person_nat, person_gen, person_age))
+#     connection.commit()
 
